2d6725d3dd8cd8df155cade199a6cbb8db26815e
Kasper <kasperbak89@pm.me>
2022-12-10 23:46:50 +0100

    Fully implement the LinkedLists methods as in TOP linked lists project

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,24 +1,3 @@
-# You will need two classes:
-
-# LinkedList class, which will represent the full list.
-# Node class, containing a #value method and a link to the #next_node, set both as nil by default.
-# Build the following methods in your linked list class:
-
-# #append(value) adds a new node containing value to the end of the list
-# #prepend(value) adds a new node containing value to the start of the list
-# #size returns the total number of nodes in the list
-# #head returns the first node in the list
-# #tail returns the last node in the list
-# #at(index) returns the node at the given index
-# #pop removes the last element from the list
-# #contains?(value) returns true if the passed in value is in the list and otherwise returns false.
-# #find(value) returns the index of the node containing value, or nil if not found.
-# #to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
-# Extra Credit
-# #insert_at(value, index) that inserts a new node with the provided value at the given index.
-# #remove_at(index) that removes the node at the given index.
-# Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their #next_node link updated.
-
 class LinkedList
   attr_reader :head, :tail
 
@@ -137,21 +116,13 @@ class LinkedList
     @head = current_node.next_node and return "#{current_node.value} was removed at index #{idx}" if idx == 0
     until current_node.next_node.nil?
       if index == idx - 1
-        value = current_node.next_node.value
-        p value
         current_node.next_node = current_node.next_node.next_node
-        return "#{value} was removed at index #{idx}"
+        return "#{current_node.next_node.value} was removed at index #{idx}"
       end
       current_node = current_node.next_node
       index += 1
     end
   end
-  # Pseudo code for remove_at
-  # Given a index remove the value at that index:
-  # go through the list until just before the wanted index
-  # the current_node next_node becomes the next_nodex.next_node
-  #
-
 end
 
 class Node
